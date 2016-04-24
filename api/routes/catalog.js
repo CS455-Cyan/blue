418582cabd06f91f228ee870acf7d3a3b560e7d2
Andrew Fisher <andrewcfisher2010@gmail.com>
2016-04-24 15:58:18 -0500

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch dev  Your branch is ahead of 'origin/dev' by 2 commits.    (use "git push" to publish your local commits)
     Changes to be committed:
    	new file:   TestFile.txt
    	modified:   package.json
    	modified:   routes/catalog.js

--- api/routes/catalog.js
+++ api/routes/catalog.js
@@ -20,6 +20,8 @@ var privilege = {
 	primaryAdmin: 5,
 	secondaryAdmin: 2
 }
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 // connect to mongoose
 db.mongoose.connect('mongodb://cyan:8029df8b@ds035603.mongolab.com:35603/apps');
@@ -1565,12 +1567,255 @@ router.put
 	}
 );
 
+/*
+	Route: Change password
+	Input:
+		payload: {"password": String}
+	Output:
+
+	Created: 04/24/2016 Andrew Fisher
+	Modified:
+ */
+router.put
+(
+	'/admin/password',
+	function(req, res)
+	{
+		if(isAuthenticated(appname, privilege.secondaryAdmin, req.session, res))
+		{
+			db.models.Admin.update({ author: req.session.username}, 
+			{ $set: req.body }).exec(function(err, request){
+				var success = err ? false : true;
+				res.send({success: success});
+			});
+		}
+	}
+);
+
+
+/*
+	Route: View change requests (created by that admin)
+	Input:
+	Output:
+		{"success": Boolean, 
+		 "data": {
+			"_id": String,
+			"author": String,
+			"timeOfRequest": Date,
+			"timeOfApproval": Date,
+			"status": String,
+			"requestTypes": [],
+			"newCourseInfo": {
+				"syllabusFile": String,
+				"title": String,
+				"name": String,
+				"description": String,
+				"number": String,
+				"hours": String,
+				"fee": String,
+				"prerequisitesCorequisites": String,
+				"offerings": []
+			},
+			"revisedFacultyCredentials": {
+				"needed": Boolean,
+				"content": String
+			},
+			"courseListChange": {
+				"needed": Boolean,
+				"content": String
+			},
+			"effective": {
+				"semester": String,
+				"year": String
+			},
+			"courseFeeChange": String,
+			"affectedDepartmentsPrograms": String,
+			"approvedBy": String,
+			"description": String,
+			"comment": String
+		}}
+	Created: 04/24/2016 Andrew Fisher
+	Modified:
+ */
+router.get
+(
+	'/admin/changeRequests/userRequests',
+	function(req, res)
+	{
+		// restrict this to primary and secondary admins
+		if(isAuthenticated(appname, privilege.secondaryAdmin, req.session, res))
+		{
+			db.models.ChangeRequest.find({author: req.session.username}).exec(function(err, results){
+					var success = err ? false : true;
+					res.send({success: success, data: results});
+			});
+		}
+	}
+);
+
+/*
+	Route: Create change request
+	Input:
+		upload:	file
+	Output:
+
+	Created: 04/24/2016 Andrew Fisher
+	Modified:
+ */
+router.post
+(
+	'/admin/changeRequests/userRequests',
+	upload.single('file'),
+	function(req, res)
+	{
+		// restrict this to primary and secondary admins
+		if(isAuthenticated(appname, privilege.secondaryAdmin, req.session, res))
+		{
+			if(req.session.privilege >= privilege.primaryAdmin){
+				req.body.status = "approved";
+			}
+			else{
+				req.body.status = "pending";
+			}
+			req.body.author = req.session.username;
+			new db.models.ChangeRequest(req.body).save(function(err){
+					var success = err ? false : true;
+					res.send({success: success});
+				});
+			var storage = multer.diskStorage({
+				destination: function (req, file, cb) {
+					cb(null, 'uploads/catalog')
+				},
+				filename: function (req, file, cb) {
+					cb(null, Date.now())
+				}
+			})
+			var upload = multer({ storage: storage })
+		}
+	}
+);
+
+/*
+	Route: Edit change request
+	Input:
+		payload: {"effective" {
+					"semester": String,
+					"year": String
+					}}
+	Output:
+
+	Created: 04/24/2016 Andrew Fisher
+	Modified:
+ */
+router.put
+(
+	'/admin/changeRequests/userRequests/:id',
+	function(req, res)
+	{
+		// restrict this to primary and secondary admins
+		if(isAuthenticated(appname, privilege.secondaryAdmin, req.session, res))
+		{
+			db.models.ChangeRequest.update({_id: req.params.id, status: "pending"}, 
+			{ $set: req.body }).exec(function(err, request){
+				var success = err ? false : true;
+				res.send({success: success});
+			});
+		}
+	}
+);
+
+/*
+	Route: Remove change request
+	Input:
+		url parameters:
+			id: id of course
+	Output:
+
+	Created: 04/24/2016 Andrew Fisher
+	Modified:
+ */
+router.delete
+(
+	'/admin/changeRequests/userRequests/:id',
+	function(req, res)
+	{
+		// restrict this to primary and secondary admins
+		if(isAuthenticated(appname, privilege.secondaryAdmin, req.session, res))
+		{
+			db.models.ChangeRequest.remove({_id: req.params.id, status: "pending"}).exec(function(err){
+					var success = err ? false : true;
+					res.send({success: success});
+			});
+		}
+	}
+);
+
+/*
+	Route: View change log
+	Input:
+	Output:
+		{"success": Boolean, 
+		 "data": {
+			"_id": String,
+			"author": String,
+			"timeOfRequest": Date,
+			"timeOfApproval": Date,
+			"status": String,
+			"requestTypes": [],
+			"newCourseInfo": {
+				"syllabusFile": String,
+				"title": String,
+				"name": String,
+				"description": String,
+				"number": String,
+				"hours": String,
+				"fee": String,
+				"prerequisitesCorequisites": String,
+				"offerings": []
+			},
+			"revisedFacultyCredentials": {
+				"needed": Boolean,
+				"content": String
+			},
+			"courseListChange": {
+				"needed": Boolean,
+				"content": String
+			},
+			"effective": {
+				"semester": String,
+				"year": String
+			},
+			"courseFeeChange": String,
+			"affectedDepartmentsPrograms": String,
+			"approvedBy": String,
+			"description": String,
+			"comment": String
+		}}
+	Created: 04/24/2016 Andrew Fisher
+	Modified:
+ */
+router.get
+(
+	'/admin/changeRequests/log',
+	function(req, res)
+	{
+		// restrict this to primary and secondary admins
+		if(isAuthenticated(appname, privilege.secondaryAdmin, req.session, res))
+		{
+			db.models.ChangeRequest.find({status: "approved"}).exec(function(err, results){
+					var success = err ? false : true;
+					res.send({success: success, data: results});
+			});
+		}
+	}
+);
 
 /*
 	Route: View change request queue
 	Input:
 	Output:
-		{"success": Boolean, data: {
+		{"success": Boolean, 
+		 "data": {
 			"_id": String,
 			"author": String,
 			"timeOfRequest": Date,
